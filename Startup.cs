cb558532fdca85e1e7b6f38884c8ae1f83022326
JackyTsai70113 <j70113jacky@yahoo.com.tw>
2020-08-13 00:09:59 +0800

    delete services

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,6 @@ namespace webapiProject {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddScoped<ICourseService, CourseService>();
-
             // Add OpenAPI v3 document
             services.AddOpenApiDocument();
 
