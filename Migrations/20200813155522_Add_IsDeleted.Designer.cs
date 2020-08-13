55ead647248887d5ead250ea718ee5dbcfbdfbf5
JackyTsai70113 <j70113jacky@yahoo.com.tw>
2020-08-13 23:55:44 +0800

    Migration新增 Add_IsDeleted

--- Migrations/20200813155522_Add_IsDeleted.Designer.cs
+++ Migrations/20200813155522_Add_IsDeleted.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webapiProject.Models;
 
 namespace webapiProject.Migrations
 {
     [DbContext(typeof(ContosoUniversityContext))]
-    partial class ContosoUniversityContextModelSnapshot : ModelSnapshot
+    [Migration("20200813155522_Add_IsDeleted")]
+    partial class Add_IsDeleted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
